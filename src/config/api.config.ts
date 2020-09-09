eea4e96ba6bbdf5f80d0cd21dfefdd954250fbfb
Rui Cunha <rui_filipe-5@hotmail.com>
2020-09-09 13:12:07 +0100

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://curso-spring-ionic12345.s3.eu-west-2.amazonaws.com"
 }
